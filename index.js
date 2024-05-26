d1ff547ef86cb243a90fe3cba6e986a594ef0b8d
Amraprayashimaar <146368594+Amraprayashimaar@users.noreply.github.com>
2024-05-26 11:44:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM Content Loaded"); 
+
+    
+    let loginButton = document.getElementById('btn');
+    loginButton.onclick = function() {
+        console.log("Button clicked"); 
+        window.location.href = "index.new.html"; 
+    };
+     });
+     document.addEventListener("DOMContentLoaded",function(){
+        let butto =document.getElementById('Drum');
+        butto.addEventListener('click',btn);
+         });
+         function btn(){
+           window. location.href=("index-create.html")
+         }
+         document.addEventListener("DOMContentLoaded", function() {
+          console.log("DOM Content Loaded"); 
+      
+          
+          let cart = document.getElementById('botn');
+          cart.onclick = function() {
+              console.log("Button clicked"); 
+              window.location.href = "cart.html"; 
+            };
+             });
